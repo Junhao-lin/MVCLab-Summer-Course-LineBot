42cd8ec3a822c07b607ed13026235fe1207c47ec
Junhao-lin <xxz3155@gmail.com>
2022-08-24 12:06:22 +0800

    add accounting

--- README.md
+++ README.md
@@ -1,22 +1,43 @@
 # LineBot
 ## How to use
 1. Install Python Package
-  > pip install -r requirements.txt
-2. Run ngrok
-  > uvicorn sport:app --reload
-3. Test LineBot 
-  > python main.py 
+  ```
+  pip install -r requirements.txt
+  ```
+2. Install InfluxDB
+```
+  sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+  sudo echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+  sudo apt update
+  sudo apt install influxdb
+```
+3. Run ngrok
+```
+  uvicorn sport:app --reload
+```
+4. Test LineBot 
+```
+  python main.py
+```
 ## Indtroduction
-Calculator
-- operator
-  - `+`
-  - `-`
-  - `*`
-  - `/`
-- input (a,b can be negative)
-  - `a+b`
-  - `a + b`
-  - `a+b =` 
-- output
-  - `calculation result` 
-  - If you enter illegal characters or don't match the rules you will get the picture
+### Calculator
+  - operator
+    - `+`
+    - `-`
+    - `*`
+    - `/`
+  - input (a,b can be negative)
+    - `a+b`
+    - `a + b`
+    - `a+b =` 
+  - output
+    - `calculation result` 
+    - If you enter illegal characters or don't match the rules you will get the picture
+
+### Accounting
+
+  - `#note [事件] [+/-] [錢]`                     -> 新增記帳資料
+  - `#report`                                    -> 顯示目前記帳資料
+  - `#delete [i]`                                -> 刪除第i筆資料
+  - `#drop`                                      -> 刪除全部資料
+  - `#sum`                                       -> 結算一天前的帳
--- main.py
+++ main.py
@@ -22,12 +22,49 @@ from fastapi import FastAPI, Request, HTTPException
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+from influxdb import InfluxDBClient
+"""
+init DB
+"""
+class DB():
+    def __init__(self, ip, port, user_id, password, db_name):
+        self.client = InfluxDBClient(ip, port, user_id, password, db_name)
+        print('Influx DB init.....')
+
+    def insertData(self, data):
+        """
+        [data] should be a list of datapoint JSON,
+        "measurement": means table name in db
+        "tags": you can add some tag as key
+        "fields": data that you want to store
+        """
+
+        if self.client.write_points(data):
+            return True
+        else:
+            print('Falied to write data')
+            return False
+
+    def queryData(self, query):
+        """
+        [query] should be a SQL like query string
+        """
+        return self.client.query(query)
+# Init a Influx DB and connect to it
+db = DB('0.0.0.0', 8086, 'root', '', 'accounting_db')
+
 load_dotenv()
 app = FastAPI()
 
 CHANNEL_TOKEN = os.environ.get('LINE_TOKEN')
 CHANNEL_SECRET = os.getenv('LINE_SECRET')
-CHANNEL_ID = os.getenv('LINE_UID') 
+
+My_LineBotAPI = LineBotApi(CHANNEL_TOKEN) # Connect Your API to Line Developer API by Token
+handler = WebhookHandler(CHANNEL_SECRET) # Event handler connect to Line Bot by Secret key
+'''
+For first testing, you can comment the code below after you check your linebot can send you the message below
+'''
+CHANNEL_ID = os.getenv('LINE_UID') # For any message pushing to or pulling from Line Bot using this ID
 def ChekExpression(string):
     check_result = True   # 標誌位
 
@@ -44,10 +81,8 @@ def ChekExpression(string):
         check_result = False
 
     return check_result
-My_LineBotAPI = LineBotApi(CHANNEL_TOKEN) # Connect Your API to Line Developer API by Token
-handler = WebhookHandler(CHANNEL_SECRET) # Event handler connect to Line Bot by Secret key
 
-# Line Developer webhook Entry powerpoint
+# Line Developer Webhook Entry Point
 @app.post('/')
 async def callback(request: Request):
     body = await request.body() # Get request
@@ -56,24 +91,200 @@ async def callback(request: Request):
         handler.handle(body.decode('utf-8'), signature) # Handler handle any message from LineBot and 
     except InvalidSignatureError:
         raise HTTPException(404, detail='LineBot Handle Body Error !')
+
     return 'OK'
+# Events for message reply
+my_event = ['#calculator', '#help', '#note', '#report', '#delete', '#drop','#sum']
 # All message events are handling at here !
 @handler.add(MessageEvent, message=TextMessage)
 def handle_textmessage(event):
-    message = TextSendMessage(text= event.message.text)
-    message.text = re.sub("=","",message.text)
-    if ChekExpression(message.text):
+    recieve_message = str(event.message.text).split(' ')
+    case_ = recieve_message[0].lower().strip()
+    #Case 1: calculator
+    if re.match(my_event[0], case_):
+        if len(recieve_message) < 1:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(text = '請輸入數值' )
+            )
+        else:
+            message = recieve_message[1]
+            message = re.sub("=","",message)
+            if ChekExpression(message):
+                My_LineBotAPI.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text = eval(message))
+                )
+            else:
+                url = 'https://truth.bahamut.com.tw/s01/201908/e9e7205639db85adff92d071cb44f997.PNG'
+                My_LineBotAPI.reply_message(
+                    event.reply_token,
+                    ImageSendMessage(
+                        original_content_url=url,
+                        preview_image_url=url)
+                )
+    # Case 2: help
+    elif re.match(my_event[1], case_):
+        command_describtion = 'Commands:\n\
+        #calculator <formula>\n\t-->four fundamental operations of arithmetic\n\
+        #note [事件] [+/-] [錢]\n\t-->add new event\n\
+        #report \n\t-->show all event\n\
+        #delete <number>\n\t-->Delete an event\n\
+        #drop \n\t-->Delete all event\n\
+        #sum \n\t-->print one day total money'
         My_LineBotAPI.reply_message(
             event.reply_token,
-            TextSendMessage(text = eval(message.text))
+            TextSendMessage(text=command_describtion)
         )
+
+    # Case 3: note
+    elif re.match(my_event[2], case_):
+        # cmd: #note [事件] [+/-] [錢]
+        if len(recieve_message) != 4:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(
+                    text="Wrong input"
+                )
+            )
+            return
+        event_ = recieve_message[1]
+        op = recieve_message[2]
+        money = int(recieve_message[3])
+        # process +/-
+        if op == '-':
+            money *= -1
+        # get user id
+        user_id = event.source.user_id
+        # build data
+        data = [
+            {
+                "measurement" : "accounting_items",
+                "tags": {
+                    "user": str(user_id),
+                    # "category" : "food"
+                },
+                "fields":{
+                    "event": str(event_),
+                    "money": money
+                }
+            }
+        ]
+        if db.insertData(data):
+            # successed
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(
+                    text="Write to DB Successfully!"
+                )
+            )
+    # Case 4:report
+    elif re.match(my_event[3], case_):
+        # get user id
+        user_id = event.source.user_id
+        query_str = """
+        select * from accounting_items
+        """
+        result = db.queryData(query_str)
+        points = result.get_points(tags={'user': str(user_id)})
+        print(result)
+        reply_text = ''
+        for i, point in enumerate(points):
+            time = point['time']
+            event_ = point['event']
+            money = point['money']
+            reply_text += f'[{i}] -> [{time}] : {event_}   {money}\n'
+        if len(reply_text) != 0:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(
+                    text=reply_text
+                )
+            )
+        else:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(
+                    text='No data'
+                )            
+    # Case 5: delete
+    elif re.match(my_event[4], case_):
+        if len(recieve_message)!=2:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(
+                    text='Wrong input'
+                )
+            )
+            return
+        user_id = event.source.user_id
+        items = recieve_message[1]
+        query_str = """
+        select * from accounting_items
+        """
+        result = db.queryData(query_str)
+        points = result.get_points(tags={'user': str(user_id)})
+        time_tmp = []
+        for i, point in enumerate(points):
+            time = point['time']
+            time_tmp.append(time)      
+
+        if int(items) == 0 and len(time_tmp) == 1:
+            db.queryData('drop measurement accounting_items')
+        elif int(items) == 0 and len(time_tmp) != 1:
+            db.queryData(f"delete from accounting_items where time < '{time_tmp[int(items) + 1]}' -1ms")
+        elif int(items) == len(time_tmp) - 1:
+            db.queryData(f"delete from accounting_items where time > '{time_tmp[int(items) -1]}' +1ms")
+        else:
+            db.queryData(f"delete from accounting_items where time < '{time_tmp[int(items) + 1]}' -1ms and time > '{time_tmp[int(items) -1]}' +1ms")  
+
+        result = db.queryData(query_str)
+        points = result.get_points(tags={'user': str(user_id)})
+        reply_text = ''
+        for i, point in enumerate(points):
+            time = point['time']
+            event_ = point['event']
+            money = point['money']
+            reply_text += f'[{i}] -> [{time}] : {event_}   {money}\n'
+        if len(reply_text) != 0:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(
+                    text=reply_text
+                )
+            )
+        else:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(
+                    text='No data'
+                )
+            )
+    # Case 6: deleteall
+    elif re.match(my_event[5], case_):
+        db.queryData('drop measurement accounting_items')
+    # Case 7: sum  
+    elif re.match(my_event[6], case_):
+        query_str = """
+            select * from accounting_items where time > now() - 1d 
+        """
+        user_id = event.source.user_id        
+        result = db.queryData(query_str)
+        points = result.get_points(tags={'user': str(user_id)})
+        total = 0
+        for i, point in enumerate(points):
+            money = point['money']
+            total += money
+        My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(
+                    text= f"Total spend today : {total} 元"
+                )
+            )
     else:
-        url = 'https://truth.bahamut.com.tw/s01/201908/e9e7205639db85adff92d071cb44f997.PNG'
         My_LineBotAPI.reply_message(
             event.reply_token,
-            ImageSendMessage(
-                original_content_url=url,
-                preview_image_url=url)
+            TextSendMessage(text='Welcome to my pokedex ! Enter "#help" for commands !')
         )
 if __name__ == '__main__':
     import uvicorn
